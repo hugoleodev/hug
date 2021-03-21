109565af6e8d678d8e8011d2cfa69e9bed581b02
Hugo Leonardo <hugoleodev@gmail.com>
2021-03-21 12:26:31 -0300

    refactor(lexer_test): add some basic asserts

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,35 +7,49 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	t.Run("basic tokens support", func(t *testing.T) {
+		input := `=+(){},;`
 
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
+		tests := []struct {
+			expectedType    token.TokenType
+			expectedLiteral string
+		}{
+			{token.ASSIGN, "="},
+			{token.PLUS, "+"},
+			{token.LPAREN, "("},
+			{token.RPAREN, ")"},
+			{token.LBRACE, "{"},
+			{token.RBRACE, "}"},
+			{token.COMMA, ","},
+			{token.SEMICOLON, ";"},
+		}
+
+		l := New(input)
+
+		for i, tt := range tests {
+			tk := l.NextToken()
+
+			assertNthTokenType(t, i, tt.expectedType, tk.Type)
+			assertNthTokenLiteral(t, i, tt.expectedLiteral, tk.Literal)
+		}
+	})
+
+}
+
+func assertNthTokenType(t *testing.T, nth int, expected, got token.TokenType) {
+	t.Helper()
+
+	if got != expected {
+		t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+			nth, expected, got)
 	}
+}
 
-	l := New(input)
+func assertNthTokenLiteral(t *testing.T, nth int, expected, got string) {
+	t.Helper()
 
-	for i, tt := range tests {
-		tk := l.NextToken()
-
-		if tk.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tk.Type)
-		}
-
-		if tk.Literal != tt.expectedLiteral {
-			t.Fatalf("testes[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedLiteral, tk.Literal)
-		}
+	if got != expected {
+		t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+			nth, expected, got)
 	}
 }
