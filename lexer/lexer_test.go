a806b50b59ec02dd9dc5e13b2f448a465be5c493
Hugo Leonardo <hugoleodev@gmail.com>
2021-03-21 12:49:53 -0300

    test(lexer): add support to structures
    - adds support to identifiers, keywwords and numbers

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -34,6 +34,70 @@ func TestNextToken(t *testing.T) {
 		}
 	})
 
+	t.Run("support to identifiers, keywwords and numbers", func(t *testing.T) {
+		input := `let thirteen = 13;
+		let nine = 9;
+		
+		let add = fn(x, y) {
+			x + y;
+		};
+
+		let result = add(thirteen, nine);
+		`
+
+		tests := []struct {
+			expectedType    token.TokenType
+			expectedLiteral string
+		}{
+			{token.LET, "let"},
+			{token.IDENT, "thirteen"},
+			{token.ASSIGN, "="},
+			{token.INT, "13"},
+			{token.SEMICOLON, ";"},
+			{token.LET, "let"},
+			{token.IDENT, "nine"},
+			{token.ASSIGN, "="},
+			{token.INT, "9"},
+			{token.SEMICOLON, ";"},
+			{token.LET, "let"},
+			{token.IDENT, "add"},
+			{token.ASSIGN, "="},
+			{token.FUNCTION, "fn"},
+			{token.LPAREN, "("},
+			{token.IDENT, "x"},
+			{token.COMMA, ","},
+			{token.IDENT, "y"},
+			{token.RPAREN, ")"},
+			{token.LBRACE, "{"},
+			{token.IDENT, "x"},
+			{token.PLUS, "+"},
+			{token.IDENT, "y"},
+			{token.SEMICOLON, ";"},
+			{token.RBRACE, "}"},
+			{token.SEMICOLON, ";"},
+			{token.LET, "let"},
+			{token.IDENT, "result"},
+			{token.ASSIGN, "="},
+			{token.IDENT, "add"},
+			{token.LPAREN, "("},
+			{token.IDENT, "thirteen"},
+			{token.COMMA, ","},
+			{token.IDENT, "nine"},
+			{token.RPAREN, ")"},
+			{token.SEMICOLON, ";"},
+			{token.EOF, ""},
+		}
+
+		l := New(input)
+
+		for i, tt := range tests {
+			tk := l.NextToken()
+
+			assertNthTokenType(t, i, tt.expectedType, tk.Type)
+			assertNthTokenLiteral(t, i, tt.expectedLiteral, tk.Literal)
+		}
+	})
+
 }
 
 func assertNthTokenType(t *testing.T, nth int, expected, got token.TokenType) {
