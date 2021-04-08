d3cffe6fb08a7ce4d3cf7c2dbca660957e3828f2
Hugo Leonardo <hugoleodev@gmail.com>
2021-04-08 15:57:10 -0300

    feat(parser): add let statement error handling

--- parser/parser.go
+++ parser/parser.go
@@ -1,26 +1,41 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/hugoleodev/hug/ast"
 	"github.com/hugoleodev/hug/lexer"
 	"github.com/hugoleodev/hug/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
-
+	l         *lexer.Lexer
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	p.nextToken()
 	p.nextToken()
 
 	return p
 
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -87,6 +102,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
